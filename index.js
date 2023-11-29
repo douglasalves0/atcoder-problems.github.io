cfac101dd4c46692bfe3ffffa33794647edda36b
Douglas Alves de Sousa <douglas.sousa@ccc.ufcg.edu.br>
2023-11-29 10:13:50 -0300

    filtrando problemas mais recentes primeiro

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 async function init(){
     let button = document.getElementById('filter-button');
-    console.log(button);
     button.addEventListener('click', async function(){
         let minRating = document.getElementById("min-rating").value;
         let maxRating = document.getElementById("max-rating").value;
@@ -22,17 +21,19 @@ async function filterProblems(minRating, maxRating){
     });
     let problemsList = document.getElementById("problems-list");
     problemsList.innerHTML = "";
-    for(const problemId in problemsRatings){
-        if(problemsRatings[problemId].difficulty < minRating) continue;
-        if(problemsRatings[problemId].difficulty > maxRating) continue;
-        if(problemsRatings[problemId].difficulty == null) continue;
+    let keysInReverse = Object.keys(problemsRatings).reverse();
+    keysInReverse.forEach(problemId => {
+        if(problemsRatings[problemId].difficulty < minRating) return;
+        if(problemsRatings[problemId].difficulty > maxRating) return;
+        if(problemsRatings[problemId].difficulty == null) return;
+        if(problems[problemId] == undefined) return;
         let a = document.createElement("a");
         a.href = problems[problemId].url;
         let li = document.createElement("li");
         li.appendChild(document.createTextNode(problems[problemId].name + " - Rating: " + problemsRatings[problemId].difficulty));
         a.appendChild(li);
         problemsList.appendChild(a);
-    }
+    });
 }
 
 window.onload = init;
